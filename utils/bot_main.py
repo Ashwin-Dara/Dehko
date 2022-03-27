3135a70d6931c0763dda4b6ce11fa6657a0ef1fd
Ashwin Dara <adara@berkeley.edu>
2022-03-26 23:30:43 -0700

    Added some rough design ideas for the message flow. It is broken up into four components. Check the file 'bot_main.py' for more details.

--- utils/bot_main.py
+++ utils/bot_main.py
@@ -4,3 +4,32 @@
 # https://discordpy.readthedocs.io/en/stable/quickstart.html
 # Previous REPL Project on Data Pipelines
 # Should not be too difficult. Making the API calls and using the proper data structure will be the hardest part.
+
+# Overview of Message Flow
+"""
+---------- RECEIVING
+
+1. Recieve a message from the users within the server.
+2. Check if the message is directed towards the bot.
+    - We will be able to do this via a special message structure.
+    - @bot_name message...
+3. If the message is directed towards the bot, we need to do some processing
+
+---------- PROCESSING
+
+1. Remove the @bot_name from the message string
+2. If anything is enclosed around ~ https//:youtube.com... ~, it is a link. Store is as a corresponding "argument" pair
+    - remove the link from the message
+3. Classify the message.
+
+----------- RESPONSE
+
+1. Perform the appropriate action. Send Follow up messages if needed.
+2. For the weather, think of a way to extract the city name out of the current text. May be difficult and need some autocorrelation.
+3. Use the NLPModel class and the CommandModel class
+
+------------ ANALYZE
+1. Store the command that was called and the associated user
+2. Store key phrases (similar to the zkie bot project)
+
+"""
