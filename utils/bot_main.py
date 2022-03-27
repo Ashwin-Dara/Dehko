3da5fa65aa4c3592404fe6e50a1532740805f29a
Ashwin Dara <adara@berkeley.edu>
2022-03-26 23:13:03 -0700

    Read through documentation for tomorrow.io API and discord.py API. Created notes. Will need to create a design doc.

---
+++ utils/bot_main.py
@@ -0,0 +1,6 @@
+# import discord.py
+
+# Useful References
+# https://discordpy.readthedocs.io/en/stable/quickstart.html
+# Previous REPL Project on Data Pipelines
+# Should not be too difficult. Making the API calls and using the proper data structure will be the hardest part.
