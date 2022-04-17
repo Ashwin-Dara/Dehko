5bdbc29f094e37f07c2a7acf8e1c085e8fbe7544
Ashwin Dara <adara@berkeley.edu>
2022-04-16 23:12:15 -0700

    Added drafting for the website. Use cartoon work lol.

---
+++ utils/yt_post_header.py
@@ -0,0 +1,30 @@
+header = """POST /youtubei/v1/log_event?alt=json&key=AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX30 HTTP/1.1
+Host: music.youtube.com
+User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0
+Accept: */*
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate, br
+Content-Type: application/json
+Content-Length: 1029
+X-Goog-Visitor-Id: CgtXRUM0TUVySDRvNCj7xe6SBg%3D%3D
+X-Goog-AuthUser: 1
+x-origin: https://music.youtube.com
+X-YouTube-Client-Name: 67
+X-YouTube-Client-Version: 1.20220411.01.00
+X-YouTube-Device: cbr=Firefox&cbrver=96.0&ceng=Gecko&cengver=96.0&cos=X11&cplatform=DESKTOP
+X-Youtube-Identity-Token: QUFFLUhqbFZ2OGJ6RmhXb01UV1hxc2JhdnY1aXJQbGVUUXw=
+X-YouTube-Page-CL: 440836743
+X-YouTube-Page-Label: youtube.music.web.client_20220411_01_RC00
+X-YouTube-Utc-Offset: -420
+X-YouTube-Time-Zone: America/Los_Angeles
+X-YouTube-Ad-Signals: dt=1650172668126&flash=0&frm&u_tz=-420&u_his=1&u_h=1080&u_w=1920&u_ah=1003&u_aw=1920&u_cd=24&bc=31&bih=452&biw=1908&brdim=0%2C27%2C0%2C27%2C1920%2C27%2C1920%2C1003%2C1920%2C452&vis=1&wgl=true&ca_type=image
+Origin: https://music.youtube.com
+Sec-Fetch-Dest: empty
+Sec-Fetch-Mode: cors
+Sec-Fetch-Site: same-origin
+Authorization: SAPISIDHASH 1650172720_c9ef67bb7e6b6f69db9987c796f728e0e8256c8d
+Referer: https://music.youtube.com/
+Alt-Used: music.youtube.com
+Connection: keep-alive
+Cookie: VISITOR_INFO1_LIVE=WEC4MErH4o4; PREF=tz=America.Los_Angeles&f5=30000&f6=40000000&f4=4000000; __Secure-3PSID=Igj_M4Ak9Mw6xydmm1r3VGE2PIlzJIPRDCmQpUYALRu-zevBZlGrGtsY7y6hkJIZL1isQQ.; __Secure-3PAPISID=UynAa-g4rdG1YlUW/AemfTjclPeEm7NhGr; __Secure-3PSIDCC=AJi4QfGJuyov0eQaXugN-BEQSfcH_x7AeB4v9oArzaQ_AzJAFLPl_kDx5DbD6-ooVQwnzMG4pA; SID=Igj_M4Ak9Mw6xydmm1r3VGE2PIlzJIPRDCmQpUYALRu-zevB62YJkS-rmKoF_sbvHUWw5A.; __Secure-1PSID=Igj_M4Ak9Mw6xydmm1r3VGE2PIlzJIPRDCmQpUYALRu-zevBHZMS2UmRkYqWmJApYojflA.; HSID=ADVguSht2ML7yL0Hq; SSID=AgWUuuiFer9E8ygVL; APISID=CLsTM1_X2NL4aw1-/AChjZYIGUYu8OEuJ5; SAPISID=UynAa-g4rdG1YlUW/AemfTjclPeEm7NhGr; __Secure-1PAPISID=UynAa-g4rdG1YlUW/AemfTjclPeEm7NhGr; LOGIN_INFO=AFmmF2swRAIgDs1_5MMBE0vvHCpiT7yJEehuQKUAgJXnnc_91l5C460CICX5rXwyNUlOyQSM4f9J-fgcfeYC2bhcIDN4gZKSHUmd:QUQ3MjNmejBQX0QxaElaR1pvUTJUd3lHZ2ZyU2RQdzBNMGJXUlJ3cmxzOHc0REtPWTdGTm5OT1pvUmM0ZUpRRC1ZUHBuTW5HU2NCMGpuZ2NtclozTVAxbFpzOEI0SWRsRUtnSFlqZ1V1MkRCSlljbThPcjhqUUJKTHZibjhySzJzaWQwcGQxLS1JcjQ4X1c1UFZLT0FRT0JoZTROYnplRUJB; SIDCC=AJi4QfG8xbk9Ru19Jw3vhQjXHUPmXYCJfK_oKZv9KRlKrVSyKiqcex-zWBw2C6BMUEv-V9jKQw; YSC=NOv5_NUetqA; _gcl_au=1.1.706281229.1650172664
+"""
--- utils/yt_utils.py
+++ utils/yt_utils.py
@@ -1,7 +1,13 @@
 import sys
+import json
+from yt_post_header import *
 sys.path.append('../ytmusicapi')
+sys.path.append('../headers_auth_private.json')
 
+from ytmusicapi import YTMusic
+YTMusic.setup('headers_auth_private.json', headers_raw=header)
 # Official Documentation for YouTube Music API
+yt_music = YTMusic()
 
 def play_music(link):
     pass
